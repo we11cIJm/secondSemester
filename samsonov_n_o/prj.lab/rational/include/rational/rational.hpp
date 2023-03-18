111f08f5153b0a60659f1c464c3bae99c020068b
Nikolay <n0.samsonov29@gmail.com>
2023-03-18 22:15:33 +0300

    added some inits

--- samsonov_n_o/prj.lab/rational/include/rational/rational.hpp
+++ samsonov_n_o/prj.lab/rational/include/rational/rational.hpp
@@ -1,21 +1,30 @@
-#pragma once
 #ifndef RATIONAL
 #define RATIONAL
 
 #include <iosfwd>
+#include <sstream>
 #include <cstdint>
 
 class Rational
 {
 public:
     Rational();
-    Rational(int32_t rhs);
+    Rational(const int32_t rhs);
+    Rational(Rational&&) = default;
     Rational(const int32_t lhs, const int32_t rhs);
     Rational(const Rational& rhs);
 
-    int32_t Nod(int32_t lhs, int32_t rhs);
-    void Reduce();
-    // int32_t ToInt();
+    ~Rational() = default;
+
+    static const char separator{'/'};
+    
+    Rational& operator=(const Rational& rhs);
+	Rational& operator=(Rational&&) = default;
+    
+    std::ostream& WriteTo(std::ostream& ostrm) const;
+    std::istream& ReadFrom(std::istream& istrm);
+	
+    Rational operator-();
     Rational& operator+=(const Rational& rhs);
     Rational& operator-=(const Rational& rhs);
     Rational& operator*=(const Rational& rhs);
@@ -25,36 +34,34 @@ public:
     Rational operator++(int);
     Rational& operator--();
     Rational operator--(int);
+
+    int32_t Nod(int32_t lhs, int32_t rhs);
+    void Reduce();
+    
     int32_t & GetNum() { return num; }
     int32_t & GetDen() { return den; }
-
-    static const char separator{ '/' };
-
-    std::ostream& WriteTo(std::ostream& ostrm) const;
-    std::istream& ReadFrom(std::istream& istrm);
-
-    ~Rational() = default;
 
 private:
     int32_t num{0};
     int32_t den{1};
+
 };
 
 std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs);
 std::istream& operator>>(std::istream& istrm, Rational& rhs);
 
 Rational operator+(const Rational& lhs, const Rational& rhs);
-Rational operator+(const Rational& lhs, const int32_t rhs);
-Rational operator+(const int32_t lhs, const Rational& rhs);
+Rational operator+(const Rational& lhs, const int32_t rhs); //
+Rational operator+(const int32_t lhs, const Rational& rhs); //
 Rational operator-(const Rational& lhs, const Rational& rhs);
-Rational operator-(const Rational& lhs, const int32_t rhs);
-Rational operator-(const int32_t lhs, const Rational& rhs);
+Rational operator-(const Rational& lhs, const int32_t rhs); // 
+Rational operator-(const int32_t lhs, const Rational& rhs); //
 Rational operator*(const Rational& lhs, const Rational& rhs);
-Rational operator*(const Rational& lhs, const int32_t rhs);
-Rational operator*(const int32_t lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const int32_t rhs); // 
+Rational operator*(const int32_t lhs, const Rational& rhs); //
 Rational operator/(const Rational& lhs, const Rational& rhs);
-Rational operator/(const Rational& lhs, const int32_t rhs);
-Rational operator/(const int32_t lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const int32_t rhs); // 
+Rational operator/(const int32_t lhs, const Rational& rhs); //
 
 bool operator==(Rational lhs, const Rational& rhs);
 bool operator!=(Rational lhs, const Rational& rhs);
