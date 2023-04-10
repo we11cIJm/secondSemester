bda00d9d23ff25e12326ab265c64b7c588098443
Nikolay <n0.samsonov29@gmail.com>
2023-04-11 01:58:04 +0300

    fix some defects and edited 'operator='

--- samsonov_n_o/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ samsonov_n_o/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -30,15 +30,15 @@ public:
     // void push_back(const double& lvalue) noexcept;
 
 private:
-    std::ptrdiff_t ssize_{0};
-    std::ptrdiff_t realSize_{0};
+    std::ptrdiff_t size_{0};
+    std::ptrdiff_t capacity_{0};
     T* data_{nullptr};
 };
 
 template<typename T>
 ArrayT<T>::ArrayT()
-    : ssize_(0),
-    realSize_(0),
+    : size_(0),
+    capacity_(0),
     data_(nullptr)
 {}
 
@@ -47,36 +47,33 @@ ArrayT<T>::ArrayT(const std::ptrdiff_t size) {
     if (size < 0) {
         throw std::invalid_argument("size cannot be negative");
     } else if (size == 0) {
-        ssize_ = 0;
-        realSize_ = 0;
+        size_ = 0;
+        capacity_ = 0;
         data_ = nullptr;
     } else {
-        ssize_ = size;
-        realSize_ = ssize_ * 2;
-        data_ = new T[realSize_]{};
-        for (ptrdiff_t i = 0; i < ssize_; ++i) {
-            data_[i] = 0;
-        }
+        size_ = size;
+        capacity_ = size_ * 2;
+        data_ = new T[capacity_]{};
     }
 }
 
+// may be different type of array?
 template<typename T>
 ArrayT<T>::ArrayT(const ArrayT& arrInp)
-    : ssize_(arrInp.ssize_),
-    realSize_(arrInp.ssize_ * 2) {
-        data_ = new T[realSize_]{};
-        for(ptrdiff_t i = 0; i < arrInp.ssize_; ++i) {
-            data_[i] = arrInp.data_[i];
-        }
+    : size_(arrInp.size_), capacity_(arrInp.size_ * 2) {
+    data_ = new T[capacity_]{};
+    for(std::ptrdiff_t i = 0; i < arrInp.size_; ++i) {
+        data_[i] = arrInp.data_[i];
+    }
 }
 
 template<typename T>
 ArrayT<T>::ArrayT(const std::ptrdiff_t size, const T& rvalue)
     :
-    ssize_(size),
-    realSize_(ssize_ * 2) {
-        data_ = new T[realSize_]{};
-        for (int i = 0; i < ssize_; ++i) {
+    size_(size),
+    capacity_(size_ * 2) {
+        data_ = new T[capacity_]{};
+        for (std::ptrdiff_t i = 0; i < size_; ++i) {
             data_[i] = rvalue;
         }
 }
@@ -84,11 +81,11 @@ ArrayT<T>::ArrayT(const std::ptrdiff_t size, const T& rvalue)
 template<typename T>
 ArrayT<T>& ArrayT<T>::operator=(const ArrayT& rhs) {
     if (this != &rhs) {
-        ssize_ = rhs.ssize_;
-        if (this->realSize_ < rhs.realSize_) {
-            realSize_ = rhs.realSize_;
-        }
-        for (ptrdiff_t i = 0; i < rhs.ssize(); ++i) {
+        delete[] data_;
+        size_ = rhs.size_;
+        capacity_ = rhs.size_ * 2;
+        data_ = new T[capacity_]{};
+        for (std::ptrdiff_t i = 0; i < rhs.size_; ++i) {
             data_[i] = rhs.data_[i];
         }
     }
@@ -97,7 +94,7 @@ ArrayT<T>& ArrayT<T>::operator=(const ArrayT& rhs) {
 
 template<typename T>
 T& ArrayT<T>::operator[](const std::ptrdiff_t index) {
-    if (index >= ssize_ || index < 0) {
+    if (index >= size_ || index < 0) {
         throw std::invalid_argument("out of range");
     }
     return *(data_ + index);
@@ -105,15 +102,15 @@ T& ArrayT<T>::operator[](const std::ptrdiff_t index) {
 
 template<typename T>
 const T ArrayT<T>::operator[](const std::ptrdiff_t index) const {
-    if (index >= ssize_ || index < 0) {
+    if (index >= size_ || index < 0) {
         throw std::invalid_argument("out of range");
     }
     return *(data_ + index);
 }
 
 template<typename T>
-std::ptrdiff_t ArrayT<T>::ssize()const noexcept {
-    return ssize_;
+std::ptrdiff_t ArrayT<T>::ssize() const noexcept {
+    return size_;
 }
 
 template<typename T>
@@ -121,30 +118,26 @@ void ArrayT<T>::resize(const std::ptrdiff_t newSize) {
     if (newSize <= 0) {
         throw std::invalid_argument("size cannot be negative or null");
     }
-    if (newSize > realSize_) {
-        realSize_ = newSize * 2;
-        T *pNewData = new T[realSize_]{};
-        for (ptrdiff_t i = 0; i < this->ssize_; ++i) {
+    if (newSize > capacity_) {
+        capacity_ = newSize * 2;
+        T *pNewData = new T[capacity_]{};
+        for (std::ptrdiff_t i = 0; i < this->size_; ++i) {
             pNewData[i] = data_[i];
         }
-        for (ptrdiff_t i = 0; i < newSize; ++i) {
-            pNewData[i] = 0;
-        }
-        delete [] data_;
+        delete[] data_;
         data_ = pNewData;
     }
-    ssize_ = newSize;
+    size_ = newSize;
 }
 
 template<typename T>
 void ArrayT<T>::insert(const std::ptrdiff_t index, const T& value) {
-    if (ssize_ + 1 <= realSize_) {
-        ssize_ += 1;
+    if (size_ + 1 <= capacity_) {
+        size_ += 1;
     } else {
-        resize(ssize_ + 1);
+        resize(size_ + 1);
     }
-    
-    for (ptrdiff_t i = ssize_; i > index; --i) {
+    for (std::ptrdiff_t i = size_; i > index; --i) {
         data_[i] = data_[i-1];
     }
     data_[index] = value;
@@ -152,13 +145,13 @@ void ArrayT<T>::insert(const std::ptrdiff_t index, const T& value) {
 
 template<typename T>
 void ArrayT<T>::remove(const std::ptrdiff_t index) {
-    if (index >= ssize_ || index < 0) {
+    if (index >= size_ || index < 0) {
         throw std::invalid_argument("out of range");
     }
-    for (ptrdiff_t i = index; i < ssize_; ++i) {
+    for (std::ptrdiff_t i = index; i < size_; ++i) {
         data_[i] = data_[i+1];
     }
-    ssize_ -= 1;
+    size_ -= 1;
 }
 
 #endif
