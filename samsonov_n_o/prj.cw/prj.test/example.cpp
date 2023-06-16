91a1e2bc4e3fea763b11c9eb8135d57d25663e53
Nikolay <n0.samsonov29@gmail.com>
2023-06-17 02:27:29 +0300

    litteraly no changes

--- samsonov_n_o/prj.cw/prj.test/example.cpp
+++ samsonov_n_o/prj.cw/prj.test/example.cpp
@@ -3,34 +3,33 @@
 
 using namespace lockFreeQueue;
 using namespace lockFreeQueueProcessor;
-using namespace parser;
 
 #ifndef NTASKS
 #define NTASKS 1000
 #endif
 
 int main(int argc, char* argv[]) {
-    parse(argc, argv);
-    LfQueue<int> queue(bufsize);
-    Consumed.clear();
+    if(!parser::parse(argc, argv)) return 0;
+    LfQueue<int> queue(parser::bufsize);
+    parser::Consumed.clear();
 
     std::vector<std::thread> threads;
 
-    for (int i = 0; i < nprodthreads; ++i) {
-        threads.emplace_back(produce, std::ref(queue), prodtime);
+    for (int i = 0; i < parser::nprodthreads; ++i) {
+        threads.emplace_back(produce, std::ref(queue), parser::prodtime);
     }
 
-    for (int i = 0; i < nconsthreads; ++i) {
-        threads.emplace_back(consume, std::ref(queue), constime);
+    for (int i = 0; i < parser::nconsthreads; ++i) {
+        threads.emplace_back(consume, std::ref(queue), parser::constime);
     }
 
     for (std::thread& thread : threads) {
         thread.join();
     }
 
-    if(Consumed.size() == NTASKS + 1 || ntasks == -1)
-        // std::cout << "everything good" << std::endl;
-        bool a = true;
-    else
-        std::cout << "smth wrong" << std::endl;
+    if(parser::Consumed.size() == NTASKS + 1 || parser::ntasks == -1) {
+        std::cout << "everything's good" << std::endl;
+    } else {
+        std::cout << "smth went wrong" << std::endl;
+    }
 }
